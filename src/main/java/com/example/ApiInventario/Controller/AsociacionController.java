5e51f2358ba55d32170a0537af64631f02799369
Bryan JImenez <bry.jimenez@duocuc.cl>
2025-05-25 13:59:19 -0400

    Agregar metodos para asociar producto_categoria

--- src/main/java/com/example/ApiInventario/Controller/AsociacionController.java
+++ src/main/java/com/example/ApiInventario/Controller/AsociacionController.java
@@ -3,6 +3,7 @@ package com.example.ApiInventario.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.example.ApiInventario.Model.Categoria;
 import com.example.ApiInventario.Model.Producto;
 import com.example.ApiInventario.Repository.CategoriaRepository;
 import com.example.ApiInventario.Repository.ProductoRepository;
+import com.example.ApiInventario.dto.AsociacionRequest;
 
 @RestController
 @RequestMapping("/api/asociaciones")
@@ -23,21 +25,19 @@ public class AsociacionController {
     private CategoriaRepository categoriaRepository;
 
 
- @PostMapping("/producto-categoria")
-    public ResponseEntity<String> asociarProductoCategoria(
-            @RequestParam Integer productoId,
-            @RequestParam Integer categoriaId) {
-        
-        Producto producto = productoRepository.findById(productoId)
-                .orElseThrow(() -> new RuntimeException("Producto no encontrado"));
-        
-        Categoria categoria = categoriaRepository.findById(categoriaId)
-                .orElseThrow(() -> new RuntimeException("Categoría no encontrada"));
-        
-        producto.setCategoria(categoria);
-        productoRepository.save(producto);
-        
-        return ResponseEntity.ok("Producto asociado a categoría exitosamente");
-    }
-
+@PostMapping("/producto-categoria")
+public ResponseEntity<String> asociarProductoCategoria(
+        @RequestBody AsociacionRequest request) {
+    
+    Producto producto = productoRepository.findById(request.getProductoId())
+            .orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+    
+    Categoria categoria = categoriaRepository.findById(request.getCategoriaId())
+            .orElseThrow(() -> new RuntimeException("Categoría no encontrada"));
+    
+    producto.setCategoria(categoria);
+    productoRepository.save(producto);
+    
+    return ResponseEntity.ok("Asociación exitosa");
+}
 }
