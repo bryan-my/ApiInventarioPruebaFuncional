1217fbfdf84ef88ed459777de2c5114ef0e345b0
Bryan JImenez <bry.jimenez@duocuc.cl>
2025-06-26 20:31:11 -0400

    Manejo de errores con las rutas de hateoas en categoria

--- src/main/java/com/example/ApiInventario/Controller/CategoriaController.java
+++ src/main/java/com/example/ApiInventario/Controller/CategoriaController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 
 import com.example.ApiInventario.Model.Categoria;
@@ -35,14 +33,14 @@ public class CategoriaController {
     }
     // Obtener categoria por ID (GET)
     @GetMapping("/{id}")
-    public ResponseEntity<?> getById(@PathVariable Integer categoria_id) {
+    public ResponseEntity<?> getById(@PathVariable("id") Integer categoria_id) {
         Categoria categoria = categoriaService.getById(categoria_id);
         if (categoria != null) {
             return ResponseEntity.ok(categoria);
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new z_Mensaje("Persona no encontrada\""));
-        }
+    }     else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new z_Mensaje("Categoría no encontrada"));
     }
+}
 
 
      // Crear nueva persona (POST)
@@ -63,29 +61,29 @@ public class CategoriaController {
 
     // Eliminar persona (DELETE)
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Integer categoria_id) {
+    public ResponseEntity<?> delete(@PathVariable("id") Integer categoria_id) {
         Categoria eliminada = categoriaService.delete(categoria_id);
         if (eliminada != null) {
             return ResponseEntity.ok(eliminada);
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Persona no encontrada");
-        }
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Categoría no encontrada");
     }
+}
 
     //METODOS HATEOAS
 
     //METODO HATEOAS para buscar por ID
     @GetMapping("/hateoas/{id}")
-    public Categoria obtenerHATEOAS(@PathVariable Integer categoria_id) {
+    public Categoria obtenerHATEOAS(@PathVariable("id") Integer categoria_id) {
         Categoria cat = categoriaService.getById(categoria_id);
-        
-        //link HATEOAS para API Gateway "A mano"
-        cat.add(Link.of("http://localhost:8888/api/proxy/productos/" + cat.getCategoria_id()).withSelfRel());
-        cat.add(Link.of("http://localhost:8888/api/proxy/productos/" + cat.getCategoria_id()).withRel("Modificar HATEOAS").withType("PUT"));
-        cat.add(Link.of("http://localhost:8888/api/proxy/productos/" + cat.getCategoria_id()).withRel("Eliminar HATEOAS").withType("DELETE"));
 
-        return cat;
-    }
+            cat.add(Link.of("http://localhost:8888/api/proxy/productos/" + cat.getCategoria_id()).withSelfRel());
+            cat.add(Link.of("http://localhost:8888/api/proxy/productos/" + cat.getCategoria_id()).withRel("Modificar HATEOAS").withType("PUT"));
+            cat.add(Link.of("http://localhost:8888/api/proxy/productos/" + cat.getCategoria_id()).withRel("Eliminar HATEOAS").withType("DELETE"));
+
+                
+            return cat;
+}
 
     //METODO HATEOAS para listar todos los productos utilizando HATEOAS
     @GetMapping("/hateoas")
